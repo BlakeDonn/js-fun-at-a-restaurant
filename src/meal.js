f0aa9f3e7367228b6ec42172388ad0154b9a3b40
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-07-02 21:09:38 -0400

    add js

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,38 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+  return `Delicious ${name}`
 }
-
+function createMenuItem(menuItemName, price, type){
+  return {
+    name: menuItemName,
+    price: price,
+    type:type
+  }
+}
+function addIngredients(ingredient, ingredients){
+  if (!ingredients.includes(ingredient)){
+    ingredients.push(ingredient);
+  }
+}
+function formatPrice(initialPrice){
+  return formattedPrice = `$${initialPrice}`;
+}
+function decreasePrice(price){
+  return (90/100)*price;
+}
+function createRecipe(title, ingredients, menuItemType){
+  return {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType,
+  }
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
+//(ingredients.indexOf(ingredient) === -1){

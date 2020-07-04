a9139af83671c6576d7105c880bb8a8044491aef
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-07-04 15:55:39 -0400

    add first three functs

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,21 @@
 class Chef {
-  constructor() {
-    
-  }
-}
+  constructor(name, restaurant){
+    this.name = name;
+    this.restaurant = restaurant;
+
+    }
+    greetCustomer(name, time){
+      if (time === true){
+        return `Good morning, ${name}!`
+      }else
+        return `Hello, ${name}!`
+    }
+    checkForFood(foodItem){
+      if (foodItem.name == foodItem){
+        return `Yes, we're serving ${fooditem.name} today!`
+      }
+    }
+
+};
 
 module.exports = Chef;
